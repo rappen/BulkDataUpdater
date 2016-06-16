3ceedb1f37320097c5b4b280ce2bb2c3b89a06d1
Jonas Rapp <jonas.rapp@cinteros.se>
2016-06-16 10:17:16 +0200

    CopyPaste-fix

--- Cinteros.XTB.BulkDataUpdater/PluginDescription.cs
+++ Cinteros.XTB.BulkDataUpdater/PluginDescription.cs
@@ -12,7 +12,7 @@
     ExportMetadata("BackgroundColor", "#ffffff"), // Use a HTML color name
     ExportMetadata("PrimaryFontColor", "#000000"), // Or an hexadecimal code
     ExportMetadata("SecondaryFontColor", "DarkGray")]
-    public class ViewDesignerTool : PluginBase
+    public class BulkDataUpdaterTool : PluginBase
     {
         #region Public Methods
 
