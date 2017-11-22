bad833c44123d061c642fdf15334b11d82ce9750
Jonas Rapp <jonas.rapp@innofactor.com>
2017-11-22 16:55:10 +0100

    Updating plugin description colors

--- Cinteros.XTB.BulkDataUpdater/PluginDescription.cs
+++ Cinteros.XTB.BulkDataUpdater/PluginDescription.cs
@@ -9,9 +9,9 @@
     ExportMetadata("Description", "Update or touch single attributes on a set of records"),
     ExportMetadata("SmallImageBase64", Constants.B64_IMAGE_SMALL), // null for "no logo" image or base64 image content
     ExportMetadata("BigImageBase64", Constants.B64_IMAGE_LARGE), // null for "no logo" image or base64 image content
-    ExportMetadata("BackgroundColor", "#ffffff"), // Use a HTML color name
-    ExportMetadata("PrimaryFontColor", "#000000"), // Or an hexadecimal code
-    ExportMetadata("SecondaryFontColor", "DarkGray")]
+    ExportMetadata("BackgroundColor", "#f8f8ff"),
+    ExportMetadata("PrimaryFontColor", "#00319C"),
+    ExportMetadata("SecondaryFontColor", "#9439B5")]
     public class BulkDataUpdaterTool : PluginBase
     {
         #region Public Methods
