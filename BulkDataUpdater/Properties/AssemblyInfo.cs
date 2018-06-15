b68d61f524ac65ed4bbb042e5ea952e6b70a2944
Jonas Rapp <jonas.rapp@innofactor.com>
2018-06-15 12:36:03 +0200

    Rename assembly ro Rappen.XTB.BDU.dll

--- BulkDataUpdater/Properties/AssemblyInfo.cs
+++ BulkDataUpdater/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bulk Data Updater")]
-[assembly: AssemblyDescription("XrmToolBox plugin to update or touch single attributes on a set of records.")]
+[assembly: AssemblyDescription("XrmToolBox plugin to update or touch attributes on a set of records.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jonas Rapp, Sweden")]
 [assembly: AssemblyProduct("Cinteros.XTB.BulkDataUpdater")]
-[assembly: AssemblyCopyright("Copyright © Innofactor 2016")]
+[assembly: AssemblyCopyright("Copyright © Jonas Rapp 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2018.6.1002")]
-[assembly: AssemblyFileVersion("1.2018.6.1002")]
+[assembly: AssemblyVersion("1.2018.6.1003")]
+[assembly: AssemblyFileVersion("1.2018.6.1003")]
